d8dd171406eb1ea5e2ceee417d2ec753836d9a7a
Daniel Franklin <danielfranklinr@gmail.com>
2024-03-10 13:18:17 -0500

    feat: Implementing retry attempts using rxjava in the datasource

--- app/src/main/java/com/dfr/inspirationquotes/data/datasource/QuoteRemoteDatasourceImpl.java
+++ app/src/main/java/com/dfr/inspirationquotes/data/datasource/QuoteRemoteDatasourceImpl.java
@@ -1,16 +1,25 @@
 package com.dfr.inspirationquotes.data.datasource;
 
+import android.util.Pair;
+
 import com.dfr.inspirationquotes.data.model.QuoteData;
 import com.dfr.inspirationquotes.data.model.mapper.QuoteDataMapper;
 import com.dfr.inspirationquotes.data.network.service.QuoteApiService;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Inject;
 
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import retrofit2.HttpException;
 
 public class QuoteRemoteDatasourceImpl implements QuoteRemoteDatasource {
 
     private final QuoteApiService quoteApiService;
+
+    private final int MAX_RETRIES = 3;
+    private final int RETRY_DELAY_SECONDS = 3;
 
     @Inject
     public QuoteRemoteDatasourceImpl(QuoteApiService quoteApiService) {
@@ -21,6 +30,24 @@ public class QuoteRemoteDatasourceImpl implements QuoteRemoteDatasource {
     public Observable<QuoteData> getRandomQuote() {
 
         return quoteApiService.fetchRandomQuote()
+                .subscribeOn(Schedulers.io())
+                .retryWhen(error -> error
+                        .zipWith(Observable.range(1, MAX_RETRIES),
+                                Pair::new)
+                        .flatMap(pair -> {
+                                    Throwable throwable = pair.first;
+                                    Integer attempt = pair.second;
+                                    if (attempt <= MAX_RETRIES
+                                            && throwable instanceof HttpException) {
+                                        return Observable.timer((long) attempt * RETRY_DELAY_SECONDS, TimeUnit.SECONDS);
+                                    } else {
+                                        return Observable.error(throwable);
+
+                                    }
+                                }
+                        )
+                )
                 .map(QuoteDataMapper::fromQuoteService);
+
     }
 }
